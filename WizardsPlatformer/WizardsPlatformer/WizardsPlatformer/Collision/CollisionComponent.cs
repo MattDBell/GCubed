93138f0f818d9efa8e9e41817d19739ff1d065c9
Matt Bell <MattDavidBell@gmail.com>
2012-11-28 16:40:36 -0800

    Look, I did collision stuff.  Way to get sick.

---
+++ WizardsPlatformer/WizardsPlatformer/WizardsPlatformer/Collision/CollisionComponent.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+delegate void CollCallBack ( CollisionComponent mine, CollisionComponent other );
+
+class CollisionComponent
+{
+    
+    List<zCollisionPrimitive> prims; //Multiple prims per coll comp
+
+}

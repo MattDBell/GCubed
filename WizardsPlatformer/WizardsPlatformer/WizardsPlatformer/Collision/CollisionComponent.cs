95136c47734014ec2df9cd4b234085797e6b1d96
Matthew Bell <MattDavidBell@Gmail.com>
2012-12-02 15:03:21 -0800

    Little bit more work to collisions.

--- WizardsPlatformer/WizardsPlatformer/WizardsPlatformer/Collision/CollisionComponent.cs
+++ WizardsPlatformer/WizardsPlatformer/WizardsPlatformer/Collision/CollisionComponent.cs
@@ -3,15 +3,31 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using Microsoft.Xna.Framework;
-delegate void CollCallBack ( CollisionComponent mine, CollisionComponent other );
+delegate void CollCallBack ( CollisionComponent mine, CollisionComponent other, CollResult coll, int collNumber );
 
 class CollisionComponent
 {
+    //This should keep an AABB, by the way.
     EntityManager.Transform myTrans;
+    CollCallBack myCallBack;
     List<zCollisionPrimitive> prims; //Multiple prims per coll comp
     void CheckCollisionsAgainst(CollisionComponent other)
     {
-        //First prim is AABB
+        int collision = 0;
+        for(int i = 0; i < prims.Count; ++i)
+        {
+            for(int y = 0; y < prims.Count; ++y)
+            {
+                CollResult c = prims[i].CheckAgainst(other.prims[y], myTrans, other.myTrans);
+                if(c.collided)
+                {
+                    myCallBack(this, other, c, collision);
+                    CollResult otherC = c;
+                    otherC.normal *= -1;
+                    other.myCallBack(other, this, otherC, collision++);
+                } //Note, currently multiple callbacks can be made, each with their own index
+            }
+        }
         
     }
 }
--- WizardsPlatformer/WizardsPlatformer/WizardsPlatformer/Collision/zCollisionPrimitive.cs
+++ WizardsPlatformer/WizardsPlatformer/WizardsPlatformer/Collision/zCollisionPrimitive.cs
@@ -7,7 +7,7 @@ using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Content;
 
 //May add more stuff later.  Also, should pool.
-class CollResult
+struct CollResult
 {
     public bool collided;
     public Vector2 normal;
